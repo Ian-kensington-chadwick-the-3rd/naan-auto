bb386929b7ccfc6d08c28fb78f965a24ed19068f
Ian Sills <iansills04@gmail.com>
2025-05-24 01:20:04 -0400

    new env

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose'); 
 require('dotenv').config()
 
-
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/naan-auto');
+// || 'mongodb://127.0.0.1:27017/naan-auto'
+mongoose.connect(process.env.MONGODB_URI );
 
 module.exports = mongoose.connection;
 

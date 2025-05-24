9dc1a89f677fc2a0dd2695ba76a42bf70aa283b6
Ian Sills <iansills04@gmail.com>
2025-05-24 01:34:42 -0400

    troubleshooting

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,15 @@
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 
 
 // || 'mongodb://127.0.0.1:27017/naan-auto'
-mongoose.connect(process.env.MONGODB_URI );
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log(process.env.MONGODB_URI)
+    })
+    .catch((error) => {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    });
 
 module.exports = mongoose.connection;
 
